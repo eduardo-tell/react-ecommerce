9a9f08715626fc4ba463205ba4bc9f5e01cf1aa6
Eduardo Tell <eduardo-tell@hotmail.com>
2026-06-29 20:27:40 -0300

    build versel

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ButtonCount from "../buttonCount/ButtonCount";
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 export default function Search({icon}) {
   // const [search, setSearch] = useState('');
