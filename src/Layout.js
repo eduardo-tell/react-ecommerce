59e92594a774df63ef2e3b99178ed5bca66b3fb9
Eduardo Tell <eduardo-tell@hotmail.com>
2026-07-05 21:33:53 -0300

    Funcionalidade fechar carrinho

--- src/Layout.js
+++ src/Layout.js
@@ -2,15 +2,6 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header";
 
 export default function Layout() {
-
-    function overlay(event) {
-      if (event) {
-        document.getElementById('body')?.classList.add('open')
-      } else {
-        document.getElementById('body')?.classList.remove('open')
-      }
-    }
-
     return (
       <>
         <Header />  
--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -9,34 +9,35 @@ export default function Cart({icon}) {
   const dialogRef = useRef(null)
   const closeButtonRef = useRef(null)
 
-  const toggleCartDrawer = (action) => {
-    return () => {
-      if (action === '1') {
-        dialogRef.current?.classList.add('open')
-      } else {
-        dialogRef.current?.classList.remove('open')
-      }
+  function toggleCartDrawer(action) {
+    if (action === '1' && !dialogRef.current?.classList.contains('open')) {
+      dialogRef.current?.classList.add('open')
+      return
     }
+
+    dialogRef.current?.classList.remove('open')
   }
 
   useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (!dialogRef.current || dialogRef.current.contains(event.target)) return
-
-      toggleCartDrawer('0')
+    const handleKeyDown = event => {
+      if (event.key === 'Escape') {
+        toggleCartDrawer()
+      }
     }
 
-    document.addEventListener('click', handleClickOutside)
+    window.addEventListener('keydown', handleKeyDown)
+    return () => window.removeEventListener('keydown', handleKeyDown)
   }, [])
 
   return (
     <>
-        <ButtonCount src={icon} count={cartProducts?.length} name="Carrinho" onClick={toggleCartDrawer('1')} />
+        <ButtonCount src={icon} count={cartProducts?.length} name="Carrinho" onClick={() => toggleCartDrawer('1')} />
 
         <div id="CartDrawer" ref={dialogRef}>
+          <div className="CartDrawer__overlay" onClick={() => toggleCartDrawer('0')}></div>
           <div className="py-8 cart-drawer__header items-center transition-all flex justify-between align-middle duration-250 ease-in-out border-b-2 border-[#e5e5e5]">
             <h3 className="text-base font-semibold">Seu Carrinho</h3>
-            <button ref={closeButtonRef} className="p-4" onClick={toggleCartDrawer('0')}>
+            <button ref={closeButtonRef} className="p-4" onClick={() => toggleCartDrawer('0')}>
               <svg part="hdt-close-icon" role="presentation" fill="none" focusable="false" width="16" height="14" viewBox="0 0 16 14">
                 <path d="M15 0L1 14m14 0L1 0" stroke="currentColor" fill="none"></path>
               </svg>
