688d64b093b3a102ace52de6862b4c785d90078a
Eduardo Tell <eduardo-tell@hotmail.com>
2026-07-09 21:52:59 -0300

    Adiciona useLocation()

--- src/components/favorites/Favorites.js
+++ src/components/favorites/Favorites.js
@@ -1,10 +1,11 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from 'react-router-dom';
 import { selectFavoritesCount } from "../../features/favorites/selectors";
 import { Button, Count } from "../buttonCount/styles.tsx";
 
 export default function Favorites({ icon, iconActive }) {
-  const favoritesCount = useSelector(selectFavoritesCount)
+  const favoritesCount = useSelector(selectFavoritesCount);
+  const location = useLocation()
 
   return (
     <Button
@@ -13,7 +14,7 @@ export default function Favorites({ icon, iconActive }) {
       className="p-2 border-transparent border-b-2 hover:border-gray-400 transition-all duration-250 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-600"
       aria-label={`Favoritos${favoritesCount > 0 ? `, ${favoritesCount} produtos` : ""}`}
     >
-      {window.location.href.includes("/favoritos") ? <img src={iconActive} alt="" width="20" height="20" /> : <img src={icon} alt="" width="20" height="20" />}
+      {location.pathname.includes("/favoritos") ? <img src={iconActive} alt="" width="20" height="20" /> : <img src={icon} alt="" width="20" height="20" />}
       {favoritesCount > 0 && <Count aria-hidden="true"> {favoritesCount} </Count>}
     </Button>
   )
