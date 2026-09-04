51c8d91f5538fc85d153e2220e8832ce1e4978a1
Eduardo Tell <eduardo-tell@hotmail.com>
2026-09-03 22:55:50 -0300

    Inclui conteudo via "createPortal"

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -5,6 +5,7 @@ import { CartDrawer } from './styles.tsx';
 import ButtonCount from "../buttonCount/ButtonCount";
 import CardItem from "../cardItem/CardItem";
 import { selectCartItems, selectCartTotal } from "../../features/cart/selectors";
+import { createPortal } from 'react-dom';
 
 export default function Cart({ icon }) {
   const cartProducts = useSelector(selectCartItems)
@@ -36,51 +37,53 @@ export default function Cart({ icon }) {
         <ButtonCount src={icon} count={cartProducts?.length} name="Carrinho" onClick={() => setIsOpen(!isOpen)} />
       </span>
 
-      <CartDrawer id="CartDrawer" ref={dialogRef} role="dialog" aria-modal="true" aria-label="Carrinho de compras">
-        <div className="CartDrawer__overlay" onClick={() => setIsOpen(!isOpen)}></div>
-        <div className="py-8 cart-drawer__header items-center transition-all flex justify-between align-middle duration-250 ease-in-out border-b-2 border-[#e5e5e5]">
-          <h3 className="text-base font-semibold">Seu Carrinho</h3>
-          <button
-            ref={closeButtonRef}
-            className="p-4 focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-600"
-            onClick={() => setIsOpen(!isOpen)}
-            aria-label="Fechar carrinho"
-          >
-            <svg part="hdt-close-icon" role="presentation" fill="none" focusable="false" width="16" height="14" viewBox="0 0 16 14">
-              <path d="M15 0L1 14m14 0L1 0" stroke="currentColor" fill="none"></path>
-            </svg>
-          </button>
-        </div>
-        <div className="cart-drawer__inner flex flex-col justify-between align-top transition-all duration-250 ease-in-out">
-          {cartProducts?.length > 0 ? (
-            <>
-              <div className="flex flex-col gap-4 w-full">
-                {cartProducts.map(cartProduct => {
-                  cartProduct = { ...cartProduct, className: "card-product-inside" }
-                  return (
-                    <CardItem key={cartProduct.id} product={cartProduct} />
-                  )
-                })}
-              </div>
+      {createPortal(
+        <CartDrawer id="CartDrawer" ref={dialogRef} role="dialog" aria-modal="true" aria-label="Carrinho de compras">
+          <div className="CartDrawer__overlay" onClick={() => setIsOpen(!isOpen)}></div>
+          <div className="py-8 cart-drawer__header items-center transition-all flex justify-between align-middle duration-250 ease-in-out border-b-2 border-[#e5e5e5]">
+            <h3 className="text-base font-semibold">Seu Carrinho</h3>
+            <button
+              ref={closeButtonRef}
+              className="p-4 focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-600"
+              onClick={() => setIsOpen(!isOpen)}
+              aria-label="Fechar carrinho"
+            >
+              <svg part="hdt-close-icon" role="presentation" fill="none" focusable="false" width="16" height="14" viewBox="0 0 16 14">
+                <path d="M15 0L1 14m14 0L1 0" stroke="currentColor" fill="none"></path>
+              </svg>
+            </button>
+          </div>
+          <div className="cart-drawer__inner flex flex-col justify-between align-top transition-all duration-250 ease-in-out">
+            {cartProducts?.length > 0 ? (
+              <>
+                <div className="flex flex-col gap-4 w-full">
+                  {cartProducts.map(cartProduct => {
+                    cartProduct = { ...cartProduct, className: "card-product-inside" }
+                    return (
+                      <CardItem key={cartProduct.id} product={cartProduct} />
+                    )
+                  })}
+                </div>
 
-              <div className="cart-drawer__footer border-t-2 border-[#e5e5e5] pt-4 mt-4">
-                <p className="flex justify-between font-semibold text-lg">
-                  <span>Total</span>
-                  <span>R${cartTotal.toFixed(2)}</span>
-                </p>
-                <Link
-                  to="/checkout"
-                  className="mt-4 block text-center transition-all bg-[#A3F7BF] text-b rounded-md py-3 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 hover:bg-[#29A29D] hover:text-white focus-visible:outline-blue-600"
-                >
-                  Finalizar compra
-                </Link>
-              </div>
-            </>
-          ) : (
-            <p className="text-center py-8">Seu carrinho está vazio.</p>
-          )}
-        </div>
-      </CartDrawer>
+                <div className="cart-drawer__footer border-t-2 border-[#e5e5e5] pt-4 mt-4">
+                  <p className="flex justify-between font-semibold text-lg">
+                    <span>Total</span>
+                    <span>R${cartTotal.toFixed(2)}</span>
+                  </p>
+                  <Link
+                    to="/checkout"
+                    className="mt-4 block text-center transition-all bg-[#A3F7BF] text-b rounded-md py-3 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 hover:bg-[#29A29D] hover:text-white focus-visible:outline-blue-600"
+                  >
+                    Finalizar compra
+                  </Link>
+                </div>
+              </>
+            ) : (
+              <p className="text-center py-8">Seu carrinho está vazio.</p>
+            )}
+          </div>
+        </CartDrawer>
+      , document.body)}
     </>
   )
 }
